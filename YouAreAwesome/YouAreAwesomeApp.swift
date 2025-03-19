7ad2c357f23886523d439a6f9267b71e0db8d6bd
Tushar Munge <63478179+Satch-Satchafunkilus@users.noreply.github.com>
2025-03-18 19:01:52 -0500

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Tushar Munge on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
