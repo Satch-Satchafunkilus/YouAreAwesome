96f342491dc0d4b87a308d57708685bc9a7a7881
Tushar Munge <63478179+Satch-Satchafunkilus@users.noreply.github.com>
2025-03-19 22:29:29 -0500

    Added a Button, and used @State to change the message when the button is clicked.

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,16 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message: String = "I am a Programmer!"
+    
     var body: some View {
         VStack {
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("You Are Awesome!")
+            Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
+            Button("Click Me!") {
+                message = "Awesome!"
+            }
         }
         .padding()
     }
