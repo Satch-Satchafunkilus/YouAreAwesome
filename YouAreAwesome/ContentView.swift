0d8e4df685c0aaee24c8b7d88fdab4974100b8b1
Tushar Munge <63478179+Satch-Satchafunkilus@users.noreply.github.com>
2025-03-30 08:13:18 -0500

    Completed Ch. 1.10 .minimumScaleFactor and the SwiftUI .animation modifier - SwiftUI course by Prof. Gallaugher

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -15,36 +15,39 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
-            Spacer()
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+                .multilineTextAlignment(.center)
+                .minimumScaleFactor(0.5)
+                .frame(height: 100)
+                .animation(.easeInOut(duration: 0.15), value: message)
 
             Image(imageName)
                 .resizable()
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-
-            Text(message)
-                .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
-                .multilineTextAlignment(.center)
+                .animation(.default, value: imageName)
 
             Spacer()
 
             Button("Show Message") {
                 let messages = [
                     "You Are Awesome!",
+                    "Gadzookd my Friend! I am astonished at how utterly mgnificent you are!",
+                    "When the Genius Bar Needs Help, They Call You!",
                     "You Are Great!",
                     "You Are Fantastic!",
                     "Fabulous? That's You!",
                     "You Make Me Smile!",
-                    "When the Genius Bar Needs Help, They Call You!"
                 ]
 
                 message = messages[messageNumber]
-                
+
                 messageNumber += 1
-                
+
                 if messageNumber == messages.count {
                     messageNumber = 0
                 }
